1c069b0bad37bdfb7f2ece7f5b689ef18923e993
Haster1337 <hasterpro@gmail.com>
2024-10-15 13:21:05 +0400

    commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+          <img src={require("./assets/react.svg")} alt="react"/>
+      </div>
+    </>
+  )
+}
+
+
+export default App
